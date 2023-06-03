1b8393bfe6504fc29f73079aec12ea48e69097a6
Hugo Brito <hugomdmb@gmail.com>
2023-06-03 12:30:50 -0300

    feat: create middlewares

--- src/routers/client.routes.ts
+++ src/routers/client.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { clientControllers } from "../controllers";
+import { isEmailUniqueMdwr } from "../middlewares/isEmailUnique.middleware";
+import { validateDataMdwr } from "../middlewares/validateData.Middleware";
+import { clientSchemas } from "../schemas";
 
 export const clientRouter = Router()
 
-clientRouter.post('', clientControllers.create)
+clientRouter.post('', validateDataMdwr(clientSchemas.postRequest), isEmailUniqueMdwr, clientControllers.create)
 clientRouter.get('', clientControllers.read)
-clientRouter.patch('/:id', clientControllers.update)
+clientRouter.patch('/:id', validateDataMdwr(clientSchemas.update), clientControllers.update)
 clientRouter.delete('/:id', clientControllers.deleter)
--- src/routers/contact.routes.ts
+++ src/routers/contact.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { contactControllers } from "../controllers";
+import { isEmailUniqueMdwr } from "../middlewares/isEmailUnique.middleware";
+import { validateDataMdwr } from "../middlewares/validateData.Middleware";
+import { contactSchemas } from "../schemas";
 
 export const contactRouter = Router()
 
-contactRouter.post('', contactControllers.create)
+contactRouter.post('', validateDataMdwr(contactSchemas.postRequest), isEmailUniqueMdwr, contactControllers.create)
 contactRouter.get('', contactControllers.read)
-contactRouter.patch('/:id', contactControllers.update)
+contactRouter.patch('/:id', validateDataMdwr(contactSchemas.patch), isEmailUniqueMdwr, contactControllers.update)
 contactRouter.delete('/:id', contactControllers.deleter)
